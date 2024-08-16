36466586eef197a084029c8b1fbefa361a792b0c
Aman Dwivedi <amandwivedi71124.com>
2024-08-16 17:43:44 +0530

    added

--- src/components/Main/HomeSlider.jsx
+++ src/components/Main/HomeSlider.jsx
@@ -11,17 +11,17 @@ function MySlider() {
   const slides = [
     {
       image: 'images/Homepage/slide1.jpg',
-      headText: 'India’s only home made end-to-end energy monitoring and management solution',
+      headText: 'India’s only indegenious end-to-end energy monitoring and management solution',
       descText: 'Adya Smart metering solutions'
     },
     {
       image: 'images/Homepage/slid2.jpg',
-      headText: 'Upgrade your Digital Meters to Smart Prepaid Chekout our AM+',
+      headText: 'Adya uses advanced technology to enhance energy efficiency and cut costs',
       descText: 'Adya Smart metering solutions'
     },
     {
       image: 'images/Homepage/slid3.jpg',
-      headText: 'Enabling Sustainable Energy Management Solutions for Utilities, Commercial & Industries sectors',
+      headText: 'Our products are designed for long-lasting, consistent performance and ensure precise billing for fair energy charges',
       descText: 'Adya Smart metering solutions'
     },
     {
@@ -31,13 +31,17 @@ function MySlider() {
     },
     {
       image: 'images/Homepage/slid5.jpg',
-      headText: 'Upgrade your Digital Meters to Smart Prepaid Chekout our AM+',
+      headText: 'Our advanced data analytics offer actionable insights, empowering customers to make informed energy decisions while keeping the customer data protected and secure',
       descText: 'Adya Smart metering solutions'
     },
     {
       image: 'images/Homepage/slid6.jpg',
-      headText: 'Enabling Sustainable Energy Management Solutions for Utilities, Commercial & Industries sectors',
+      headText: 'We provide flexible solutions for residential, commercial, and industrial customers with easy-to-use mobile apps for real-time energy monitoring and payments',
       descText: 'Adya Smart metering solutions'
+    },
+    {
+      image: 'images/Homepage/slid7.jpg',
+      headText: 'Our solutions help customers manage and reduce their energy expenses'
     },
     
   ];
@@ -45,11 +49,11 @@ function MySlider() {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 500,
+    speed: 4000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    //  autoplaySpeed: 1000
+    
   };
   const slidesData = [
     { id:1,component: <Slide1 /> },
